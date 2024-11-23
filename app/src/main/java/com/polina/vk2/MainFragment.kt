49e635fbf31b2045ede219c60f3facb6356b1c8b
polina <barlovapolina@gmail.com>
2024-11-23 15:05:15 +0100

    v2 +recycle

--- app/src/main/java/com/polina/vk2/MainFragment.kt
+++ app/src/main/java/com/polina/vk2/MainFragment.kt
@@ -1,23 +1,22 @@
 package com.polina.vk2
 
+import MyAdapter
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import com.polina.vk2.databinding.MainFragmentBinding
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import okhttp3.Response
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
-    private lateinit var currentUrl: String
+    private var currentUrlList = mutableListOf<String>()
+    private val adapter = MyAdapter()
     private val key = "live_ssyALQBcQkji6KfNgOluG0zk4o8vEKWKsKQrvE8NHluVE5rd2yMBWM5XlqbY4d9H"
 
     override fun onCreateView(
@@ -37,11 +36,13 @@ class MainFragment : Fragment(R.layout.main_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.progressBar.visibility = View.INVISIBLE
+        binding.recyclerView.adapter = adapter
         if (savedInstanceState != null) {
-            val url = savedInstanceState.getString("url")
-            Glide.with(this)
-                .load(url)
-                .into(binding.loadedImage)
+            val savedUrls = savedInstanceState.getStringArrayList("url")
+            if (savedUrls != null) {
+                currentUrlList = savedUrls.toMutableList()
+                adapter.setItems(currentUrlList)
+            }
         }
         binding.loadButton.setOnClickListener {
             viewLifecycleOwner.lifecycleScope.launch {
@@ -51,6 +52,7 @@ class MainFragment : Fragment(R.layout.main_fragment) {
     }
 
     private suspend fun loadData() {
+        binding.loadButton.isEnabled = false
         binding.progressBar.visibility = View.VISIBLE
         try {
             val response = RetrofitInstance().api.getImages(key)
@@ -58,21 +60,20 @@ class MainFragment : Fragment(R.layout.main_fragment) {
                 val catApiResponse = response.body()
                 if (catApiResponse != null) {
                     val imageUrl = catApiResponse[0].url
-                    currentUrl = imageUrl
-                    Glide.with(this)
-                        .load(imageUrl)
-                        .into(binding.loadedImage)
+                    currentUrlList.add(imageUrl)
+                    adapter.addItems(imageUrl)
                 }
             }
         } catch (e: Exception) {
-            binding.loadButton.text = getString(R.string.error)
+            Snackbar.make(binding.root, getString(R.string.error), Snackbar.LENGTH_SHORT).show()
         } finally {
             binding.progressBar.visibility = View.INVISIBLE
+            binding.loadButton.isEnabled = true
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString("url", currentUrl)
+        outState.putStringArrayList("url", ArrayList(currentUrlList))
     }
 }
